36da83c64954c40b91fdb06a10d1a920076db302
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2023-10-04 09:56:01 +0300

    add guiding comments

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function renderCharacters(characters) {
 	const container = document.querySelector('.characters');
 
+	// iterate through each character and append to the dom
 	characters.forEach((character) => {
 		const characterName = document.createElement('h1');
 		characterName.textContent = character.name;
@@ -29,28 +30,34 @@ async function renderCharacter(id) {
 	// Clear character details div
 	container.innerHTML = '';
 
+	// create img tag to display character image
 	const img = document.createElement('img');
 	img.src = character.image;
 	img.alt = character.name;
 	container.appendChild(img);
 
+	// create paragraph tag to display votes
 	const p = document.createElement('p');
 	p.textContent = `Votes: ${character.votes}`;
 	container.appendChild(p);
 
+	// create a button to add character vote count
 	const button = document.createElement('button');
 	button.textContent = 'Add vote';
 	button.addEventListener('click', () => {
 		character.votes++;
+		// persist vote count to db.json
 		updateCharacter(character);
 		p.textContent = `Votes: ${character.votes}`;
 	});
 	container.appendChild(button);
 
+	// create button to reset character vote count
 	const resetButton = document.createElement('button');
 	resetButton.textContent = 'Reset votes';
 	resetButton.addEventListener('click', () => {
 		character.votes = 0;
+		// persist the reset vote count to db.json
 		updateCharacter(character);
 		p.textContent = `Votes: ${character.votes}`;
 	});
@@ -76,19 +83,23 @@ function updateCharacter(character) {
 function getCharacters() {
 	fetch(BASE_URL)
 		.then((res) => res.json())
+		// passing the renderCharacters function as a callback
 		.then(renderCharacters)
+		// handle any errors during a fetch request
 		.catch((err) => {
 			console.log(err);
 		});
 }
 
 // Get a single character
+// using async/await syntax
 async function getCharacter(id) {
 	try {
 		const res = await fetch(`${BASE_URL}/${id}`);
 
 		return res.json();
 	} catch (error) {
+		// handle any errors during a fetch request
 		console.log(error);
 	}
 }
